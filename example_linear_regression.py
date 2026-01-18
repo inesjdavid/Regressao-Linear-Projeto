5664a7cd4cde88f6f3514a820a5b7309194580dd
ines <ines13david@gmail.com>
2026-01-18 01:13:25 +0000

    Refined example plots layout

--- example_linear_regression.py
+++ example_linear_regression.py
@@ -18,9 +18,6 @@ model = LinearRegression(
 
 model.fit(X, Y)
 
-#equação da regressão aprendida
-#print(f"\nLearned regression model:Y = {model.coef_[0]:.3f} * X + {model.intercept_:.3f}")
-
 #previsão para novos dados
 X_new = np.array([[12], [14], [16]])
 y_pred_new = model.predict(X_new)
@@ -35,57 +32,46 @@ print()
 print(f"Iterations: {model.n_iter_}")
 print(f"Stop reason: {model.stop_reason_}")
 
-#gráfico dos resultados
+#gráfico linear regression
 r2 = model.score(X, Y) #calcular erro
 
 plt.figure(figsize=(9, 5))
 plt.gca().set_axisbelow(True)
-plt.scatter(X, Y, label="Training data")
+plt.scatter(X, Y, color="darkorange", label="Training data")
 
 X_line = np.linspace(0, 16, 100).reshape(-1, 1)
 y_line = model.predict(X_line)
 plt.plot(X_line, y_line, label="Regression Line")
-plt.scatter(
-    X_new,
-    y_pred_new,
-    color="deeppink",
-    s=70,
-    label="New predictions",
-    zorder=3)
+plt.scatter(X_new, y_pred_new, color="deeppink", s=70, label="New predictions", zorder=3)
+
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Linear Regression Example")
+plt.legend(loc="upper left", bbox_to_anchor=(1.02, 1.0))
+plt.grid(True)
 
 info_text = (
     f"y = {model.coef_[0]:.2f}·X + {model.intercept_:.2f}\n"
     f"$R^2$ = {r2:.3f}")
 
 plt.text(
-    0.01, 1.10,
+    1.06, 0.78,
     info_text,
     transform=plt.gca().transAxes,
     ha="left",
+    va="top",
     bbox=dict(boxstyle="round", edgecolor="deeppink", facecolor="white"))
 
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.title("Linear Regression Example")
-plt.legend(loc="center left", bbox_to_anchor=(1.02, 0.5))
-plt.grid(True)
-plt.subplots_adjust(right=0.75, top=0.80)
+plt.subplots_adjust(right=0.75)
 plt.savefig("example_linear_regression.png", dpi=150)
 plt.close()
-    
-#output error inicial vs final
-initial_loss = model.loss_history_[0]
-final_loss = model.loss_history_[-1]
-print("\nTraining error summary:")
-print(f"  Initial MSE: {initial_loss:.4f}")
-print(f"  Final MSE:   {final_loss:.4f}")
     
 #graf da evolução do erro (loss)
 initial_loss = model.loss_history_[0]
 final_loss = model.loss_history_[-1]
 
 plt.figure(figsize=(7, 4))
-plt.plot(model.loss_history_)
+plt.plot(model.loss_history_, color="deeppink")
 plt.yscale("log")
 
 mse_text = (
@@ -97,7 +83,7 @@ plt.text(
     mse_text,
     transform=plt.gca().transAxes,
     ha="left",
-    bbox=dict(boxstyle="round", facecolor="white"))
+    bbox=dict(boxstyle="round", edgecolor="deeppink", facecolor="white"))
 
 plt.xlabel("Iteration")
 plt.ylabel("Mean Squared Error")
